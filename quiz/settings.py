3827cea2b518b8a8710c7faab8488a7fdec0a76e
Vitor <vtloboss@hotmail.com>
2021-11-24 21:56:54 -0300

    COnfigurado collectstatic

--- quiz/settings.py
+++ quiz/settings.py
@@ -117,6 +117,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
+
+COLLECTFAST_ENABLED = False
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
